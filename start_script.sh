06b40c97e60767d7c347927b05254ae0a7396815
Valeriya Zaborovskaya <102747534+valerizabby@users.noreply.github.com>
2024-05-21 23:31:59 +0300

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# установка istio
-curl -L https://istio.io/downloadIstio | sh -
-# добавляем клиент в istioctl в PATH
-export PATH="$PATH:$PWD/istio-1.21.2/bin"
-# используем демонстрационный профиль конфигурации
-istioctl install -y --set profile=demo --set meshConfig.outboundTrafficPolicy.mode=REGISTRY_ONLY
-# метка пространства имен
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.22.0 TARGET_ARCH=x86_64 sh -
+cd istio-1.22.0
+export PATH=$PWD/bin:$PATH
+istioctl install --set profile=demo -y --set meshConfig.outboundTrafficPolicy.mode=REGISTRY_ONLY
 kubectl label namespace default istio-injection=enabled
+cd ..
 
-docker build -t app:v0.1 app/ -f ./app/Dockerfile.app
-docker build -t time_request:v0.1 app/ -f ./app/Dockerfile.client
+docker build -t app ./app -f ./app/Dockerfile.app
+docker build -t time_request app/ -f ./app/Dockerfile.client
+
+docker tag app valerizab/app
+docker tag time_request valerizab/time_request
+
+docker push valerizab/app
+docker push valerizab/time_request
 
 kubectl create -f app/deploy/app-service.yaml
 kubectl create -f app/deploy/app-deployment.yaml
-kubectl create -f app/deploy/cronjob.yaml
+kubectl create -f app/deploy/script-deployment.yaml
 
 kubectl create -f app/deploy/gateway.yaml
-kubectl create -f app/deploy/virtualservice.yaml
+kubectl create -f app/deploy/virtual-service.yaml
 kubectl create -f app/deploy/external-service.yaml
 
 minikube tunnel
